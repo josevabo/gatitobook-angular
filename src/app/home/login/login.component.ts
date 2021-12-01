2b938fc9f073c53a5aaa6bfb8aa416d414240290
josevabo <43576654+josevabo@users.noreply.github.com>
2021-11-30 21:32:26 -0300

    Implementando autenticacao via service

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,30 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  usuario = "";
+  senha = "";
+
+  constructor(private authService: AutenticacaoService) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      ()=>console.log('autenticado com sucesso!'),
+
+      (error)=>{
+        alert('Usuário ou senha invalidos!');
+        console.log(error)
+      }
+    )
+  }
+
+}
